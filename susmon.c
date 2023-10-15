87687ea52e84f4ad2c2eb1e9820d8475df27cd8a
dripbert <dripbert@users.noreply.github.com>
2023-10-15 19:24:56 +0200

    generalize window drawing

--- susmon.c
+++ susmon.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include "susmon.h"
 
 #ifdef __FreeBSD__
 #include <sys/sysctl.h>
@@ -15,6 +16,8 @@
 #define HEIGHT 500
 #define HSIZE  20
 #define TSIZE  16
+
+typedef void (*Contents)();
 
 typedef struct Timer {
   double start_time;
@@ -28,41 +31,75 @@ typedef struct Card {
   int y2;
   bool minimized;
   bool moving;
+  Contents contents;
+  char *title;
 } Card;
+
+Card disk = {
+  .x1 = 460,
+  .y1 = 20,
+  .x2 = 460 + 200,
+  .y2 = 20 + 200,
+  .minimized = true,
+  .moving = false,
+  .contents = disk_info,
+  .title = "DISK",
+};
+Card cpu = {
+  .x1 = 20,
+  .y1 = 20,
+  .x2 = 20 + 200,
+  .y2 = 20 + 200,
+  .minimized = false,
+  .moving = false,
+  .contents = cpu_info,
+  .title = "CPU",
+};
+Card mem = {
+  .x1 = 240,
+  .y1 = 20,
+  .x2 = 240 + 200,
+  .y2 = 20 + 200,
+  .minimized = false,
+  .moving = false,
+  .contents = mem_info,
+  .title = "MEMORY",
+};
+
+const int cpu_freq_graph_len = 20; 
+float cpu_freq_graph[cpu_freq_graph_len] = {0};
+
+void disk_info() {
+  DrawRectangle(disk.x1, disk.y1, disk.x2 - disk.x1, disk.y2 - disk.y1, DARKGRAY);
+  DrawText(TextFormat("Disk: %.1fGig/%.1f Gig", sus_disk_total(), sus_disk_total()), disk.x1 + 10, disk.y1 + 30, TSIZE, LIGHTGRAY);
+}
+void cpu_info() {
+  DrawRectangle(cpu.x1, cpu.y1, cpu.x2 - cpu.x1, cpu.y2 - cpu.y1, DARKGRAY);
+  DrawText(TextFormat("CPUs: %d", sysconf(_SC_NPROCESSORS_ONLN)), cpu.x1 + 10, cpu.y1 + 30, TSIZE, LIGHTGRAY);
+  DrawText(TextFormat("CPU perc: %d%%", sus_cpu_perc()), cpu.x1 + 10, cpu.y1 + 46, TSIZE, LIGHTGRAY);
+  DrawText(TextFormat("CPU freq: %.1f Ghz", sus_cpu_freq()), cpu.x1 + 10, cpu.y1 + 62, TSIZE, LIGHTGRAY);
+
+  int h;
+  for (int i = 0; i < cpu_freq_graph_len; ++i) {
+    h = cpu_freq_graph[i] * 10;
+    DrawRectangle(cpu.x1 + (10 * i), cpu.y2 - h, 5, h, RED);
+  }
+}
+void mem_info() {
+  DrawRectangle(mem.x1, mem.y1, mem.x2 - mem.x1, mem.y2 - mem.y1, DARKGRAY);
+  DrawText(TextFormat("Mem: %.1fGig/%.1f Gig", sus_mem_used(), sus_mem_phys()), mem.x1 + 10, mem.y1 + 30, TSIZE, LIGHTGRAY);
+}
 
 int main(void)
 {
+  Card *cards[] = {&cpu, &mem, &disk};
+  int cards_len = 3;
   InitWindow(WIDTH, HEIGHT, "susmon");
 
-  Card cpu = {
-    .x1 = 20,
-    .y1 = 20,
-    .x2 = 20 + 200,
-    .y2 = 20 + 200,
-    .minimized = false,
-    .moving = false,
-  };
-  Card mem = {
-    .x1 = 240,
-    .y1 = 20,
-    .x2 = 240 + 200,
-    .y2 = 20 + 200,
-    .minimized = false,
-    .moving = false,
-  };
-  Card disk = {
-    .x1 = 460,
-    .y1 = 20,
-    .x2 = 460 + 200,
-    .y2 = 20 + 200,
-    .minimized = true,
-    .moving = false,
-  };
+  disk.contents = disk_info;
 
   Timer graph_refresh = {.start_time = GetTime(), .duration = 5};
 
-  const int cpu_freq_graph_len = 20; 
-  float cpu_freq_graph[cpu_freq_graph_len] = {0};
 
   Vector2 mouse = { -100.0f, -100.0f };
 
@@ -94,18 +131,18 @@ int main(void)
         disk.moving = false;
     }
 
-    if (mouse.x >= disk.x2 - 20 && mouse.x <= disk.x2 && mouse.y >= disk.y1 && mouse.y <= disk.y1 + 20 && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+    if (mouse.x >= disk.x2 - 20 && mouse.x <= disk.x2 && mouse.y >= disk.y1 - 20 && mouse.y <= disk.y1 && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
       disk.minimized = !disk.minimized;
-    else if (mouse.x >= mem.x2 - 20 && mouse.x <= mem.x2 && mouse.y >= mem.y1 && mouse.y <= mem.y1 + 20 && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+    else if (mouse.x >= mem.x2 - 20 && mouse.x <= mem.x2 && mouse.y >= mem.y1 - 20 && mouse.y <= mem.y1 && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
       mem.minimized = !mem.minimized;
-    else if (mouse.x >= cpu.x2 - 20 && mouse.x <= cpu.x2 && mouse.y >= cpu.y1 && mouse.y <= cpu.y1 + 20 && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+    else if (mouse.x >= cpu.x2 - 20 && mouse.x <= cpu.x2 && mouse.y >= cpu.y1 - 20 && mouse.y <= cpu.y1 && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
       cpu.minimized = !cpu.minimized;
 
-    else if (mouse.x >= cpu.x1 && mouse.x <= cpu.x2 && mouse.y >= cpu.y1 && mouse.y <= cpu.y1 + 20 && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+    else if (mouse.x >= cpu.x1 && mouse.x <= cpu.x2 && mouse.y >= cpu.y1 - 20 && mouse.y <= cpu.y1 && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
       cpu.moving = true;
-    else if (mouse.x >= mem.x1 && mouse.x <= mem.x2 && mouse.y >= mem.y1 && mouse.y <= mem.y1 + 20 && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+    else if (mouse.x >= mem.x1 && mouse.x <= mem.x2 && mouse.y >= mem.y1 - 20 && mouse.y <= mem.y1 && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
       mem.moving = true;
-    else if (mouse.x >= disk.x1 && mouse.x <= disk.x2 && mouse.y >= disk.y1 && mouse.y <= disk.y1 + 20 && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+    else if (mouse.x >= disk.x1 && mouse.x <= disk.x2 && mouse.y >= disk.y1 - 20 && mouse.y <= disk.y1 && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
       disk.moving = true;
 
     if (GetTime() - graph_refresh.start_time > graph_refresh.duration) {
@@ -123,39 +160,16 @@ int main(void)
 
 
     // CPU INFO
-    if (!cpu.minimized) {
-      DrawRectangle(cpu.x1, cpu.y1, cpu.x2 - cpu.x1, cpu.y2 - cpu.y1, DARKGRAY);
-      DrawText(TextFormat("CPUs: %d", sysconf(_SC_NPROCESSORS_ONLN)), cpu.x1 + 10, cpu.y1 + 30, TSIZE, LIGHTGRAY);
-      DrawText(TextFormat("CPU perc: %d%%", sus_cpu_perc()), cpu.x1 + 10, cpu.y1 + 46, TSIZE, LIGHTGRAY);
-      DrawText(TextFormat("CPU freq: %.1f Ghz", sus_cpu_freq()), cpu.x1 + 10, cpu.y1 + 62, TSIZE, LIGHTGRAY);
-
-      int h;
-      for (int i = 0; i < cpu_freq_graph_len; ++i) {
-        h = cpu_freq_graph[i] * 10;
-        DrawRectangle(cpu.x1 + (10 * i), cpu.y2 - h, 5, h, RED);
+    for (int i = 0; i < cards_len; ++i){
+      Card c = *cards[i];
+      DrawRectangle(c.x1, c.y1 - 20, c.x2 - c.x1, 20, ORANGE);
+      DrawRectangle(c.x2 - 20, c.y1 - 20, 20, 20, RED);
+      DrawText(c.title, c.x1, c.y1 - 20, HSIZE, BLACK);
+      if(!c.minimized){
+        c.contents();
       }
-    }
-    DrawRectangle(cpu.x1, cpu.y1, cpu.x2 - cpu.x1, 20, BLUE);
-    DrawRectangle(cpu.x2 - 20, cpu.y1, 20, 20, RED);
-    DrawText("CPU:", cpu.x1, cpu.y1, HSIZE, BLACK);
 
-    // MEMORY INFO
-    if (!mem.minimized) {
-      DrawRectangle(mem.x1, mem.y1, mem.x2 - mem.x1, mem.y2 - mem.y1, DARKGRAY);
-      DrawText(TextFormat("Mem: %.1fGig/%.1f Gig", sus_mem_used(), sus_mem_phys()), mem.x1 + 10, mem.y1 + 30, TSIZE, LIGHTGRAY);
     }
-    DrawRectangle(mem.x1, mem.y1, mem.x2 - mem.x1, 20, GREEN);
-    DrawRectangle(mem.x2 - 20, mem.y1, 20, 20, RED);
-    DrawText("MEMORY:", mem.x1, mem.y1, HSIZE, BLACK);
-
-    // DISK INFO
-    if (!disk.minimized) {
-      DrawRectangle(disk.x1, disk.y1, disk.x2 - disk.x1, disk.y2 - disk.y1, DARKGRAY);
-      DrawText(TextFormat("Disk: %.1fGig/%.1f Gig", sus_disk_total(), sus_disk_total()), disk.x1 + 10, disk.y1 + 30, TSIZE, LIGHTGRAY);
-    }
-    DrawRectangle(disk.x1, disk.y1, disk.x2 - disk.x1, 20, YELLOW);
-    DrawRectangle(disk.x2 - 20, disk.y1, 20, 20, RED);
-    DrawText("DISK:", disk.x1, disk.y1, HSIZE, BLACK);
 
     // DrawLine(0, 240, WIDTH, 240, WHITE);
 
---
+++ susmon.h
@@ -0,0 +1,3 @@
+void disk_info();
+void cpu_info();
+void mem_info();
